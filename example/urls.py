113eb4f78379f8ae1763fa5a68b059a3e2a9dbe7
Owais Ali Khan <owaisalikhan2003@gmail.com>
2024-01-23 22:12:38 +0500

    Added bootstrap 5 CDN view.

--- example/urls.py
+++ example/urls.py
@@ -1,9 +1,10 @@
 # example/urls.py
 from django.urls import path
 
-from example.views import index
+from example.views import index, bootstrap
 
 
 urlpatterns = [
-    path('', index),
+    path("", index),
+    path("bootstrap", bootstrap),
 ]
--- example/views.py
+++ example/views.py
@@ -1,16 +1,21 @@
 # example/views.py
 from datetime import datetime
-
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def index(request):
     now = datetime.now()
-    html = f'''
+    html = f"""
     <html>
         <body>
             <h1>Hello from Vercel!</h1>
             <p>The current time is { now }.</p>
         </body>
     </html>
-    '''
+    """
     return HttpResponse(html)
+
+
+def bootstrap(request):
+    return render(request, "example/bootstrap.html")
